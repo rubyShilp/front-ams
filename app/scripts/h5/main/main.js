7f06aa0ed40a7629cd2b156737c3f8f0bb0af6d9
ax <aix@fadada.com>
2020-12-15 12:20:01 +0800

    时间查询条件设置

--- app/scripts/h5/main/main.js
+++ app/scripts/h5/main/main.js
@@ -1,8 +1,8 @@
 import charts from "@/components/charts";
 import * as mainServer from "@/pages/main/main.server.js";
-import main from '@/pages/main/main.js';
+import main from "@/pages/main/main.js";
 export default {
-  mixins:[main],
+  mixins: [main],
   components: {
     "ams-chart": charts,
   },
@@ -11,36 +11,35 @@ export default {
       dataList: [],
     };
   },
-  beforeMount(){
-    this.querystaticsA()
+  beforeMount() {
+    this.querystaticsA();
   },
   methods: {
     //数据统计列表查询
-    initStatistics(type){
-      this.querytype=type,
-      this.initSumTop();
+    initStatistics(type) {
+      (this.querytype = type), this.initSumTop();
       this.initAttendTop(1);
       // this.initAttendTop(2);
       this.initHealthTop(2);
       this.querystaticsA();
     },
     //考情异常数据统计
-    querystaticsA(){
+    querystaticsA() {
       let params = {
-        starttime: this.starttime,
-        endtime: this.endtime,
+        starttime: formDate(new Date(this.starttime), "yyyy-MM-dd hh:mm:ss"),
+        endtime: formDate(new Date(this.endtime), "yyyy-MM-dd hh:mm:ss"),
         schoolcode: this.userInfo.roles[0].schoolcode,
         gradecode: this.userInfo.roles[0].gradecode,
         classcode: this.userInfo.roles[0].classcode,
         querytype: this.querytype,
         page: 0,
-        pagesize: 100
-      }
-      mainServer.querystaticsAttend(params).then(res=>{
-        if(res.success){
-           this.dataList = res.resultMap.abAttendSumList;
+        pagesize: 100,
+      };
+      mainServer.querystaticsAttend(params).then((res) => {
+        if (res.success) {
+          this.dataList = res.resultMap.abAttendSumList;
         }
-      })
-    }
+      });
+    },
   },
 };
